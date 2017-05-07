d867770fa7aff3ac5f889e1f4fe8852ca8061058
meepingsnesroms <guicrith@gmail.com>
2017-05-06 18:49:54 -0700

    Remove build leftovers

--- build/runloop.d
+++
@@ -1,4 +0,0 @@
-runloop.o: /Volumes/compHDD/ClexaGBA/source/runloop.cpp \
- /Volumes/compHDD/ClexaGBA/source/menu.h
-
-/Volumes/compHDD/ClexaGBA/source/menu.h:
--- build/ugui.d
+++
@@ -1,7 +0,0 @@
-ugui.o: /Volumes/compHDD/ClexaGBA/source/ugui/ugui.c \
- /Volumes/compHDD/ClexaGBA/source/ugui/ugui.h \
- /Volumes/compHDD/ClexaGBA/source/ugui/ugui_config.h
-
-/Volumes/compHDD/ClexaGBA/source/ugui/ugui.h:
-
-/Volumes/compHDD/ClexaGBA/source/ugui/ugui_config.h:
